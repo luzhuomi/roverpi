9d910ecffe1c1961f0e616e1960c97f2e568f3da
Kenny Lu <luzhuomi@gmail.com>
2013-02-15 19:23:53 +0800

    first check in

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 roverpi
 =======
+
+a simple implementation of a rover using raspberry pi and PiFace
+
+
+prerequisite
+============
+
+
+demo
+====
+pi@raspberrypi ~/git/roverpi/trunk/roverpi $ sudo modprobe spi_bcm2708
+pi@raspberrypi ~/git/roverpi/trunk/roverpi $ sudo python
+Python 2.7.3 (default, Jan 13 2013, 11:20:46) 
+[GCC 4.6.3] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import rover as r
+>>> x = r.Rover()
+>>> x.forward()
+>>> x.right()
+>>> x.left()
---
+++ trunk/roverpi/rover.py
@@ -0,0 +1,29 @@
+
+import piface.pfio
+from time import sleep
+
+class Rover:
+    
+    def __init__(self):
+        piface.pfio.init()
+        
+    def forward(self,unit=5):
+        piface.pfio.digital_write(1, 1)
+        piface.pfio.digital_write(2, 1)
+        sleep(unit)
+        piface.pfio.digital_write(1, 0)
+        piface.pfio.digital_write(2, 0)
+
+    def left(self,unit=5):
+        piface.pfio.digital_write(1, 0)
+        piface.pfio.digital_write(2, 1)
+        sleep(unit)
+        piface.pfio.digital_write(1, 0)
+        piface.pfio.digital_write(2, 0)
+
+    def right(self,unit=5):
+        piface.pfio.digital_write(1, 1)
+        piface.pfio.digital_write(2, 0)
+        sleep(unit)
+        piface.pfio.digital_write(1, 0)
+        piface.pfio.digital_write(2, 0)
